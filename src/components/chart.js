b452a29be607fe999e5686c03f131df003f20fb0
Augusto José <augusto.jm.amaral1@gmail.com>
2017-05-21 17:13:50 -0300

    First release

---
+++ src/components/chart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data){
+  return Math.round(data.reduce( 
+    (previousValue, currentValue) => previousValue + currentValue ) / data.length); 
+}
+
+export default (props) => {
+  return (
+    <div>
+      <Sparklines svgHeight={120} svgWidth={180} data={props.data} >
+        <SparklinesLine color={props.color}/>
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
+  );
+}
