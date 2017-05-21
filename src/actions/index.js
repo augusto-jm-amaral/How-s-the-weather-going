7c0c0b0c48f5b42356c849daac841ffe81d0fe12
Augusto José <augusto.jm.amaral1@gmail.com>
2017-05-21 18:12:31 -0300

    Change API url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '4924daae5195b06dea21c47bd876c5a3';
-const ROOT_URL  = `https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=${API_KEY}`;
+const ROOT_URL  = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=${API_KEY}`;
 
 export const FETCH_WEATHER =  'FETCH_WEATHER';
 
